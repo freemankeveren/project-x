01ca07bad71cfdae0b7a332cba4367d43a29cfcc
Keveren Freeman <freemankeveren@gmail.com>
2017-10-09 21:20:53 -0600

    new commit

---
+++ client/src/components/TopNav.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Header } from 'semantic-ui-react';
+
+const styles = {
+  main: {
+    border: 150,
+    margin: 12,
+    top: 10,
+    right: '300px',
+    bottom:20,
+    left: '250px',
+    position: 'relative',
+    background: 'grey',
+  },
+}
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div style={styles.main}>
+        <Header as='h1' textAlign='center' style={styles.main}>Project X</Header>
+      </div>
+      );
+  }
+}
+
+export default Home;
